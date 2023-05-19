1d1c8c5b71af6eec95ee3e294588a06250ac9b7d
abrarazmain <ajmainabrar5@gmail.com>
2023-05-19 20:42:49 +0600

    created post api

--- index.js
+++ index.js
@@ -29,7 +29,16 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+      await client.connect();
+      const toyCollection = client.db("toyDB").collection("toys");
+
+
+      app.post('/toys', async (req, res) => { 
+          const toy = req.body
+          const result = await toyCollection.insertOne(toy);
+          res.send(result);
+      })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
