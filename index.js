405a38745737030fb49249e2df8bb67c28c5a76f
abrarazmain <ajmainabrar5@gmail.com>
2023-05-21 00:59:54 +0600

    added update api

--- index.js
+++ index.js
@@ -24,38 +24,82 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-      const toyCollection = client.db("toyDB").collection("toys");
-      
+    const toyCollection = client.db("toyDB").collection("toys");
 
-      app.get('/allToys/:category', async (req, res) => { 
-          const category = req.params.category;
-          if (category == 'Sports Car' || category == 'Truck' || category == 'Regular Car') {
-              const result = await toyCollection.find({ category: category }).toArray();
-              return res.send(result)
-          }
-          const result = await toyCollection.find({}).toArray()
-          console.log(category);
-          res.send(result)
-      })
-    
-      app.get('/singleToy/:id', async (req, res) => {
-        const id = req.params.id;
-      
-        try {
-          const objectId = new ObjectId(id);
-          const query = { _id: objectId };
-          const result = await toyCollection.findOne(query);
-          res.send(result);
-        } catch (error) {
-          res.status(400).send('Invalid ID');
-        }
-      });
-      
+    const indexKeys = { name: 1 };
+    const indexOptions = { name: "toyNames" };
+
+    const result = await toyCollection.createIndex(indexKeys, indexOptions);
+
+    app.get("/toyByName/:text", async (req, res) => {
+      const searchText = req.params.text;
+      const result = await toyCollection
+        .find({
+          $or: [{ name: { $regex: searchText, $options: "i" } }],
+        })
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/myToys/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await toyCollection.find({ email: email }).toArray();
+      res.send(result);
+    });
+
+    app.get("/allToys/:category", async (req, res) => {
+      const category = req.params.category;
+      if (
+        category == "Sports Car" ||
+        category == "Truck" ||
+        category == "Regular Car"
+      ) {
+        const result = await toyCollection
+          .find({ category: category })
+          .toArray();
+        return res.send(result);
+      }
+      const result = await toyCollection.find({}).limit(20).toArray();
+      console.log(category);
+      res.send(result);
+    });
+
+    app.get("/singleToy/:id", async (req, res) => {
+      const id = req.params.id;
+
+      try {
+        const objectId = new ObjectId(id);
+        const query = { _id: objectId };
+        const result = await toyCollection.findOne(query);
+        res.send(result);
+      } catch (error) {
+        res.status(400).send("Invalid ID");
+      }
+    });
 
     app.post("/toys", async (req, res) => {
       const toy = req.body;
       const result = await toyCollection.insertOne(toy);
       res.send(result);
+    });
+
+    app.put("/updateToy/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const body = req.body;
+        console.log(body);
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+           ...body
+          },
+        };
+        const result = await toyCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
     });
 
     // Send a ping to confirm a successful connection
